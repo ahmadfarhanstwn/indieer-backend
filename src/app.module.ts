af0a163bb63d825b707cc63530935e6b97fd26fc
ahmadfarhanstwn <ahmadfarhanstwn@gmail.com>
2022-12-28 21:36:23 +0700

    Setup env and db using config service

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,16 @@
 import { Module } from '@nestjs/common';
-import { DatabaseModule } from './database/database.module';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { getEnvPath } from './common/helper/env.helper';
+import { TypeOrmConfigService } from './shared/typeorm/typeorm.service';
+
+const envFilePath: string = getEnvPath(`${__dirname}/common/envs`);
 
 @Module({
-  imports: [DatabaseModule],
+  imports: [
+    ConfigModule.forRoot({ envFilePath, isGlobal: true }),
+    TypeOrmModule.forRootAsync({ useClass: TypeOrmConfigService }),
+  ],
   controllers: [],
   providers: [],
 })
